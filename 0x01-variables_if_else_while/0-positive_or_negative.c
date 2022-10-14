60178d31d410cbd72bf7029ea510bb2fcae08d16
Asakippam-Simon <simonasakipaam@gmail.com>
2022-10-14 16:31:07 -0700

    Print a random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		print("is postive %d\n", n);
+	if (n == 0)
+		print("is zero %d\n", n);
+	if (n < 0)
+		print("is negative %d\n", n);
+	return (0);
+}
