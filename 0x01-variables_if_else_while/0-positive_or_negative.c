8dbad3bd3481e22e9f495179c415db591433ccf1
Asakippam-Simon <simonasakipaam@gmail.com>
2022-10-14 16:54:45 -0700

    Print a random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
+/**
  * main - determine if number is positive or negative
  *
  * Description: program will assign a random number to the variable n each time
@@ -19,9 +19,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is postive\n", n);
-	if (n == 0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	if (n < 0)
+	else
 		printf("%d is negative\n", n);
 
 	return (0);
