f8feac5c79001a05898477101311cfba081242e2
Asakippam-Simon <simonasakipaam@gmail.com>
2022-10-15 14:22:57 -0700

    Print all combinations

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,14 +11,13 @@
 int main(void)
 {
 	int i = 0;
-
 	while (i < 48)
 	{
 		if (i < 10)
 			putchar(i + '0');
-	else if (i > 41)
-		putchar(i - 10 + 'A');
-		i++;
+		else if (i > 41)
+			putchar(i - 10 + 'A');
+			i++;
 	}
 	putchar(10);
 	return (0);
