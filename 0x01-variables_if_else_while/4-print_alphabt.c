e36855ed0370d107d627cc6880dd9b2abe27843d
Asakippam-Simon <simonasakipaam@gmail.com>
2022-10-14 18:13:41 -0700

    printlowercase letters

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,23 +9,16 @@
 
 int main(void)
 {
-	int i, j;
-
-	i = 97;
-	j = 65;
+	int i = 97;
 
 	while (i < 123)
 	{
-		putchar(i);
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
 		i++;
 	}
-
-	while (j < 91)
-	{
-		putchar(j);
-		j++;
-	}
-
 	putchar(10);
 	return (0);
 }
