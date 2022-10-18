c30b6bc5b3dba9b1a7d779a93b3e7345623cd28b
Asakippam-Simon <simonasakipaam@gmail.com>
2022-10-18 13:39:45 -0700

    Print the alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+include "main.h"
+/**
+ * main - prints alphabet in lowercase
+ * Return: Alway 0
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+		putchar('\n');
+	}
+
+	return (48);
+}
