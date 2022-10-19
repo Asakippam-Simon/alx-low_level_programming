e9b6ba24abd4e260a92d505c7b3b0c5a35324fa9
Asakippam-Simon <simonasakipaam@gmail.com>
2022-10-19 15:52:33 -0700

    Check for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * int _isalpha(int c) - checks for lowercase character.
+ * _isalpha - checks for lowercase character.
  *@c: the charater to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
