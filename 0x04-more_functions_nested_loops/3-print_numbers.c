5b74bd69f89919171899466f5673cad07f242afa
Asakippam-Simon <simonasakipaam@gmail.com>
2022-10-20 15:45:41 -0700

    Print numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for uppercase letters
+ *
+ * Return: 1 if upper, 0 otherwise
+ */
+
+void print_numbers(void)
+{
+	int nums = 0;
+
+	for (nums = 0; nums < 10; nums++)
+		_putchar(nums);
+
+	_putchar('\n');
+}
