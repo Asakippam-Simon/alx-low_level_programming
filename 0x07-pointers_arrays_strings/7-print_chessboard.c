4598b22643ac9631f62867d2066c2de283cebd67
Asakippam-Simon <simonasakipaam@gmail.com>
2022-11-01 16:04:35 -0700

    Adding chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chessboard
+ * @a: 2d array of charaters
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[2])
+{
+	int n, j;
+
+	n = 0;
+	while (n < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			printf(a[n][j]);
+			j++;
+		}
+		printf('\n');
+		n++;
+	}
+}
