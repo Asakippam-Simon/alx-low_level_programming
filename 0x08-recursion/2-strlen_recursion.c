0dbf69ca1822d86fb5bbf81813876d891975f7f3
Asakippam-Simon <simonasakipaam@gmail.com>
2022-11-02 15:45:46 -0700

    Adding length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: string to print
+ * Return: void
+ */
+
+int _strlen_recursion(char *s);
+{
+	int i = 0;
+
+	if (*s != '\0')
+	{
+		_strlen_recursion(s + 1);
+		i += 1;
+	}
+	return (i);
+}
