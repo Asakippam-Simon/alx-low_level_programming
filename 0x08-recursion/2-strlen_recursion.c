38092dad232876fd8cdb586bd29c870b2876efa6
Asakippam-Simon <simonasakipaam@gmail.com>
2022-11-02 15:49:28 -0700

    Adding length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,10 +10,12 @@ int _strlen_recursion(char *s);
 {
 	int i = 0;
 
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_strlen_recursion(s + 1);
-		i += 1;
+		return (0);
 	}
-	return (i);
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
 }
