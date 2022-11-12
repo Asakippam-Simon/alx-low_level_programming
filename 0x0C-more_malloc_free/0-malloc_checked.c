5b32aa6f00b0e51efaf3692ad1b5edff0b301df2
Asakippam-Simon <simonasakipaam@gmail.com>
2022-11-11 20:50:14 -0800

    Malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,10 +9,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *mem = malloc(b);
+	unsigned int *mem;
+
+	mem = (unsigned int*)malloc(b * sizeof(int));
 
 	if (mem == NULL)
 		exit(98);
-
-	return (mem);
+	else
+		return (mem);
 }
