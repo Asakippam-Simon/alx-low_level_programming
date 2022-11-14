33200d781b1b2afd6e753ad05f737c5b136282a3
Asakippam-Simon <simonasakipaam@gmail.com>
2022-11-14 15:27:49 -0800

    Adding print name of file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the compiled file
+ * Return: 0 SUCCESS
+ */
+
+int main(void)
+{
+	printf("Print the name of the file: %s\n", __FILE__);
+
+	return (0);
+}
