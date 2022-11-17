c47adc3943da2bdb5847a3763b48a996d847b804
Asakippam-Simon <simonasakipaam@gmail.com>
2022-11-17 13:00:43 -0800

    Adding print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(args, unsigned int);
-		printf("%d\n", x);
+		printf("%d", x);
 
 		if (separator && i != n - 1)
 		{
